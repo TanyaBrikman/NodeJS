5d92f783ac33eee001785ffe7e890d91862e7853
Tanya <tanyabrikman@mail.ru>
2023-02-09 16:42:21 +0700

    Пдключение шаблонизатора (view engine), добавление Middleware.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const morgan = require('morgan')
 
 const app = express()
 
@@ -13,29 +14,42 @@ app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`Listening port ${PORT}`)
 })
 
+app.use(morgan(':method :url :status :res[content-lenght] - :response-time ms'))
+
+app.use(express.static('styles'))
 app.get('/', (req, res) => {
-    res.render(createPath('index'))
+    const title = 'Home'
+    res.render(createPath('index'), {title})
 })
 
 app.get('/posts/:id', (req, res) => {
-    res.render(createPath('post'))
+    const title = 'Post'
+    res.render(createPath('post'), {title})
 })
 
 app.get('/posts', (req, res) => {
-    res.render(createPath('posts'))
+    const title = 'Posts'
+    res.render(createPath('posts'), {title})
 })
 
 app.get('/add-post', (req, res) => {
-    res.render(createPath('add-post'))
+    const title = 'Add post'
+    res.render(createPath('add-post'), {title})
 })
 
 app.get('/contacts', (req, res) => {
-    res.render(createPath('contact'))
+    const title = 'Contacts'
+    const contacts = [
+        {name: 'GitHub', link: 'https://github.com/TanyaBrikman' },
+        {name: 'YouTube', link: 'https://www.youtube.com/TanyaBrikman'},
+    ]
+    res.render(createPath('contact'), { contacts, title })
 })
 
 app.use((req, res) => {
+    const title = 'Error page'
     res.status(404)
-       .render(createPath('error'))
+       .render(createPath('error'), {title})
 })
 
 
